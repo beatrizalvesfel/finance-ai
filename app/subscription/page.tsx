418978db97d2242cb1fccd1d35a0c7b583719599
beatrizalvesfel <beatrizalvesfel@gmail.com>
2024-11-13 22:53:06 -0300

    feat: add login page

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,5 @@
-const Subscription = () => {
+const SubscriptionPage = () => {
   return <h1 className="flex w-screen">Subscription page</h1>;
 };
 
-export default Subscription;
+export default SubscriptionPage;
