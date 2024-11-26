68cf0c5d82bfee30fd7d65acab8b5204d6c4b9f2
beatrizalvesfel <beatrizalvesfel@gmail.com>
2024-11-26 11:13:57 -0300

    feat: ensure transaction can be updated

--- app/_actions/upsert-transaction/index.ts
+++ app/_actions/upsert-transaction/index.ts
@@ -11,6 +11,7 @@ import { upsertTransactionSchema } from "./schema";
 import { revalidatePath } from "next/cache";
 
 interface UpsertTransactionParams {
+  id?: string;
   name: string;
   amount: number;
   type: TransactionType;
@@ -25,11 +26,12 @@ export const upsertTransaction = async (params: UpsertTransactionParams) => {
   if (!userId) {
     throw new Error("Unauthorized");
   }
-  await db.transaction.create({
-    data: {
-      ...params,
-      userId,
+  await db.transaction.upsert({
+    where: {
+      id: params.id,
     },
+    update: { ...params, userId },
+    create: { ...params, userId },
   });
   revalidatePath("/transactions");
 };
